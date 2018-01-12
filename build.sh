4e80901cf452931ae70358e85d93c0627cca05b2
Stanimir Iliev <stambeto2197@gmail.com>
2018-01-12 10:42:13 +0200

    Changed project structure. Made Backend a project instead of a module

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+shellDir=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+calledFrom=$PWD
+cd $shellDir/backend
+gradle clean build
+cd ..
+mkdir tmpFolder
+mv backend/build/libs/Bank.jar tmpFolder/
+rm -rf build/*
+rm -rf backend/build/
+mv tmpFolder/Bank.jar build/
+rm -rf tmpFolder/
+cd frontend/
+npm run build
+mv build/{index.html,static/} ../build/
+rm -rf build/
+cd $calledFrom
