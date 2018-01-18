528b07de9a770f49fb07b1c8c053d6f588401358
Stanimir Iliev <stambeto2197@gmail.com>
2018-01-18 16:48:03 +0200

    Added option to see transactions history

---
+++ frontend/src/Transactions.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react'
+import { Route, Link} from 'react-router-dom'
+import TransactionPage from './TransactionPage'
+import Navigation from './Navigation'
+import axios from 'axios'
+import Loading from './Loading'
+
+
+class Transactions extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            pageSize: 20,
+            loading: false,
+            transactionsCount: 0
+        }
+    }
+
+    componentWillMount() {
+        this.setState({ loading: true })
+        axios.get('/v1/transactions/count')
+            .then(response => this.setState({
+                loading: false,
+                transactionsCount: response.data.transactionsCount
+            }))
+            .catch(error => this.setState({
+                loading: false
+            }))
+		console.log(this.state)
+    }
+
+    render() {
+        const { loading, pageSize, transactionsCount } = this.state
+        if (loading) {
+            return (<Loading />)
+        }
+        if (transactionsCount === 0) {
+            return (
+                <div className="container__accounts" >
+                    <h1 className="account_menu__header">You have no transactions yet</h1>
+                    <Link className="linkButton button--close" to="/">Close</Link>
+                </div>
+            )
+        }
+
+        const routes = []
+        const pages = transactionsCount % pageSize > 0 ? parseInt(transactionsCount / pageSize) + 1 : parseInt(transactionsCount / pageSize)
+
+        for (let i = 1; i <= pages; i++) {
+            routes.push(
+                <Route path={`/transactions/${i}`} render={() => <TransactionPage page={i} pageSize={pageSize} />} />
+            )
+        }
+
+        return (
+            <div className="container__accounts" >
+                <h1 className="account_menu__header">Transaction history</h1>
+                <div className="transactions__content">
+                    {routes}
+                </div>
+                <Navigation pages={pages} />
+                <Link className="linkButton button--close" to="/">Close</Link>
+            </div>
+        )
+    }
+}
+
+export default Transactions
